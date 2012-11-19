5a79d1ba532fd0fa526bbb2f47bc01a11d5b5d21
Samuel Kommu <samueltheophilus@gmail.com>
2012-11-19 13:45:47 -0800

    Update monitorClient.py

--- monitorClient.py
+++ monitorClient.py
@@ -1,33 +1,9 @@
-#
-#                            Cisco-Style BSD
-#                 Copyright (c) 2012, Cisco Systems, Inc
-#                          All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions are met:
-#
-# * Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer. 
-#
-# * Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation 
-# and/or other materials provided with the distribution. 
-#
-# * Neither the name of Cisco, the name of the copyright holder nor the names
-# of their respective contributors may be used to endorse or promote products
-# derived from this software without specific prior written permission. 
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+# The script examples provided by Cisco for your use are provided for reference only as a customer courtesay.  
+# They are intended to facilitate development of your own scripts and software that interoperate with Cisco switches 
+# and software.  Although Cisco has made efforts to create script examples that will be effective as aids to script 
+# or software development,  Cisco assumes no liability for or support obligations related to the use of the script 
+# examples or any results obtained using or referring to the script examples.
+
 """\n  monitorClient \n      
    \t  ... Works with monitorThread.py
    \t  ... Use:  python monitorClient.py n
